1cd4ace9e1ebd5f5dd5ade87d0220836df3dcfeb
Rasmus <2277443+kjellberg@users.noreply.github.com>
2023-06-15 20:17:05 +0200

    set color by css variables

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,9 +1,3 @@
-const colors = require('tailwindcss/colors')
-
-const PRIMARY_COLOR = 'purple'
-const SECONDARY_COLOR = 'teal'
-const NEUTRAL_COLOR = 'slate'
-
 module.exports = {
   content: [
     './app/**/*.{erb,haml,html,slim}',
@@ -13,9 +7,39 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: colors[PRIMARY_COLOR],
-        secondary: colors[SECONDARY_COLOR],
-        neutral: colors[NEUTRAL_COLOR],
+        primary: {
+          100: 'var(--color-primary-100)',
+          200: 'var(--color-primary-200)',
+          300: 'var(--color-primary-300)',
+          400: 'var(--color-primary-400)',
+          500: 'var(--color-primary-500)',
+          600: 'var(--color-primary-600)',
+          700: 'var(--color-primary-700)',
+          800: 'var(--color-primary-800)',
+          900: 'var(--color-primary-900)',
+        },
+        secondary: {
+          100: 'var(--color-secondary-100)',
+          200: 'var(--color-secondary-200)',
+          300: 'var(--color-secondary-300)',
+          400: 'var(--color-secondary-400)',
+          500: 'var(--color-secondary-500)',
+          600: 'var(--color-secondary-600)',
+          700: 'var(--color-secondary-700)',
+          800: 'var(--color-secondary-800)',
+          900: 'var(--color-secondary-900)',
+        },
+        neutral: {
+          100: 'var(--color-neutral-100)',
+          200: 'var(--color-neutral-200)',
+          300: 'var(--color-neutral-300)',
+          400: 'var(--color-neutral-400)',
+          500: 'var(--color-neutral-500)',
+          600: 'var(--color-neutral-600)',
+          700: 'var(--color-neutral-700)',
+          800: 'var(--color-neutral-800)',
+          900: 'var(--color-neutral-900)',
+        }
       }
     },
   },
