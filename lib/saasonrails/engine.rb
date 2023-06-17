614ac7c3f5781cff32bada88462ec642a5d7268a
Rasmus <2277443+kjellberg@users.noreply.github.com>
2023-06-17 16:50:27 +0200

    css

--- lib/saasonrails/engine.rb
+++ lib/saasonrails/engine.rb
@@ -7,8 +7,10 @@ module Saasonrails
     config.autoload_paths += Dir[Saasonrails::Engine.root.join("app", "frontend", "components")]
     # config.eagerload_paths += Dir[Saasonrails::Engine.root.join("app", "frontend", "components")]
 
-    config.assets.paths << Saasonrails::Engine.root.join("app", "javascript")
-    config.assets.precompile += %w[saasonrails_manifest]
+    initializer "saasonrails.assets.precompile" do |app|
+      app.config.assets.paths << Saasonrails::Engine.root.join("app", "javascript")
+      app.config.assets.precompile += %w[saasonrails_manifest]
+    end
 
     initializer "saasonrails.importmap", before: "importmap" do |app|
       app.config.importmap.paths << Engine.root.join("config/importmap.rb")
